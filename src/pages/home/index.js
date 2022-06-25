4e3ef4e310bb684b28ebcd145dd34f92c7a74fd3
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2022-06-25 23:26:50 +0100

    adding stories

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from '../../components/home/right';
+import Stories from '../../components/home/stories';
 import { useRedux } from "../../hooks/useRedux";
 import { _onLogout } from "../../redux/actions/auth/logout";
-
+import './style.css'
 const Home = () => {
   const {    account } = useRedux()
-  return <div>
+  return <div className='home'>
     <Header />
    <LeftHome user={account} />
+   <div className='home_middle'>
+    <Stories />
+   </div>
    <RightHome user={account} />
   </div>;
 }
